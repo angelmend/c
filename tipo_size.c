2a1b788b52cff0dd059252ce7306fc8ebcf7c4ad
angelmend <amendezortiz@gmail.com>
2017-01-24 17:25:42 -0600

    Subida de practica 9

--- tipo_size.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-        printf("%u\n",sizeof(char));
-        printf("%u\n",sizeof(int));
-        printf("%u\n",sizeof(unsigned int));
-        printf("%u\n",sizeof(void *));
-}
-

2e46b7c7519a2ff7ee01608402a924d26bcbccd0
angelmend <amendezortiz@gmail.com>
2017-01-24 22:42:22 -0600

    Subida de practica 3

---
+++ practica3/practica3.c
@@ -0,0 +1,22 @@
+
+
+#include<stdio.h>
+#include<limits.h>
+char* cadena="Hola, Mundo \n";
+int main ()
+
+
+{
+
+
+printf ("%u\n",SHRT_MAX);
+printf ("%u\n",UCHAR_MAX);
+printf ("%u\n",INT_MAX );
+printf ("%ld\n",LONG_MAX );
+printf ("%Ld\n",LLONG_MAX);
+
+
+return 0;
+
+
+}
